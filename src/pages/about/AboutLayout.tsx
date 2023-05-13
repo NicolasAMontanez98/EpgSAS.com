c8d6089aa31c978441db754e89fec619ec572839
Nicolas Montanez <nicomontanez98@gmail.com>
2023-05-12 20:01:29 -0500

    Change styles, fix base url

--- src/pages/about/AboutLayout.tsx
+++ src/pages/about/AboutLayout.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Gallery } from 'react-grid-gallery'
 import Lightbox from 'yet-another-react-lightbox'
 import "yet-another-react-lightbox/styles.css"
-import './aboutStyles.scss'
+import './aboutStyles.css'
 
 export const About: React.FC<any> = () => {
   const [index, setIndex] = useState(-1)
